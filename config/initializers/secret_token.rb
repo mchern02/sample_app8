e9782afc6b5a86a4792602540b4b979c829963c0
Michael Cherney <mike@calendarclouds.com>
2011-11-12 08:12:25 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fe43ab0c839af579c9e1771bfc83b6e8d3959bbda15517380236d4b426bb69142bd7e305e302bd00a22590978d04d2ce87e63804b0daf398a7839887b2b2f700'
