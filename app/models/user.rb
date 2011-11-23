9ebdf959b21eb4961ab6ecf5f28d6a176d6d3454
Michael Cherney <mike@calendarclouds.com>
2011-11-23 11:54:53 -0500

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :name :email
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  
+  validates :name,  :presence   => true,
+                    :length     => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false}
 end
 
 # == Schema Information
@@ -11,5 +19,5 @@ end
 #  email      :string(255)
 #  created_at :datetime
 #  updated_at :datetime
-#
+
 
